7739e99396afce30c7f4012ec5d2b7b58f5e6fd8
Asavelawayiza <20asavelawayiza@gmail.com>
2019-11-18 16:34:00 +0200

    tests

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -1,64 +1,80 @@
 const assert = require("assert");
 const greeting = require("../greeting");
 const pg = require("pg");
-const   Pool = pg.Pool;
+const Pool = pg.Pool;
 
 const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/mydb'
 
 const pool = new Pool({
-  connectionString
+    connectionString
 });
 
 
-describe('greeting test' , function(){
+describe('greeting test', function () {
 
     var instance = greeting(pool);
-// console.log(instance.language('Asa', 'english'));
 
-    it('should greet Asavela in English' , async function(){
+    beforeEach(async function () {
+        await pool.query("delete from allnames");
+    });
+
+
+    it('should greet Asavela in English', async function () {
 
         await instance.language('Asavela', 'English')
         let names = await instance.messaging();
-       assert.equal(names , "Hello, Asavela!");
+        assert.equal(names, "Hello, Asavela!");
 
     })
 
-    it('should greet Asavela in isiXhosa' , async function(){
+    it('should greet Asavela in isiXhosa', async function () {
 
         await instance.language('Asavela', 'Xhosa')
         let names = await instance.messaging()
         assert.equal(names, "Molo, Asavela!");
 
     })
-    
-    it('should greet Asavela in Afrikaans' , async function(){
+
+    it('should greet Asavela in Afrikaans', async function () {
         await instance.language('Asavela', 'Afrikaans')
         let names = await instance.messaging();
-         assert.deepEqual(names , "Hallo, Asavela!");
+
+        assert.deepEqual(names, "Hallo, Asavela!");
 
     })
 
 
     it('should display how many times a user have been greeted', async function () {
-      
-        await instance.language('Asavela', 'Afrikaans')
-        await instance.language('Asavela', 'Afrikaans') 
 
-        let number = await instance.getCounter('Asavela');
-        
-        assert.equal(number, 2);
+        await instance.language('Nana', 'Afrikaans')
+        await instance.language('Nana', 'Afrikaans')
+
+        let number = await instance.getCounter('Nana');
+        assert.equal(number.greet_count, 2);
     });
 
-    it('it should increment the counter when a different name is entered',async function() {
-      
-        
-        await instance.keepName('asa');
-        await instance.keepName('Asavela')
-        await instance.keepName('demi');
+    it('it should increment the counter when a different name is entered', async function () {
 
-        let counts = await instance.counter()   
-            assert.equal(counts ,3);
+        await instance.language('asa');
+        await instance.language('Asavela')
+        await instance.language('demi');
+
+        let counts = await instance.counter()
+        assert.equal(counts, 3);
     })
-    
+
+    it('it should not increment when the same name is entered', async function () {
+
+        await instance.language('Asavela');
+        await instance.language('Asavela');
+        let counters = await instance.counter()
+        assert.equal(counters, 1)
+
+
+    })
+
+    after(function () {
+        pool.end();
+    })
 
 });
