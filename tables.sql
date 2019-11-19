08a47dd0a7fa50f8d6f2f7dc17d066baaafbded9
Asavelawayiza <20asavelawayiza@gmail.com>
2019-11-19 14:47:59 +0200

    table

--- tables.sql
+++ tables.sql
@@ -1,8 +1,6 @@
 create table allnames(
     id serial not null primary key,
-    location text not null,
     greet_count int not null,
-    greet_name text not null,
-    help boolean not null,
-    information text
+    greet_name text not null
+   
 );
